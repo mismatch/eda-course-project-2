f4ac400680eb5c3a53b8d16c7a18ff36e4968add
Vitaly <mismatch@yandex.ru>
2014-07-26 19:30:28 +0300

    Added script and image of plot5

--- plot5.R
+++ plot5.R
@@ -10,27 +10,26 @@ ensureDataDownloaded <- function(url) {
     file.remove(localPath)
 }
 
-getCoalCodes <- function() {
+getMotorVehicleCodes <- function() {
     srcCodes <- readRDS("Source_Classification_Code.rds")
-    coalCodesSelector <- grepl("Coal", srcCodes$EI.Sector) & 
-        grepl("Combustion", srcCodes$SCC.Level.One)
-    srcCodes$SCC[coalCodesSelector]
+    motorVehicleCodesSelector <- grepl("[Mm]obile", srcCodes$EI.Sector)
+    srcCodes$SCC[motorVehicleCodesSelector]
 }
 
-getNEIByCoalCodes <- function(coalCodes) {
+getMotorVehicleEmissions <- function(codes) {
     NEI <- readRDS("summarySCC_PM25.rds")
-    NEI[NEI$SCC %in% coalCodes, ]
+    NEI[NEI$SCC %in% codes & NEI$fips == "24510", ]
 }
 
 ensureDataDownloaded("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip")
-NEI <- getNEIByCoalCodes(getCoalCodes())
+NEI <- getMotorVehicleEmissions(getMotorVehicleCodes())
 aggregatedNEI <- aggregate(Emissions ~ year, data = NEI, sum)
 
-png("plot4.png", width = 480, height = 480)
+png("plot5.png", width = 480, height = 480)
 par(mar  = c(5, 5, 4, 1))
 
 barplot(aggregatedNEI$Emissions, col = "sienna", 
-        main = "Total emissions by year\nCoal combustion-related sources", 
+        main = "Total emissions in Baltimore City\nMotor vehicle-related sources", 
         xlab = "Year", ylab = "Emissions, tons", names = aggregatedNEI$year, 
         space = 0.075)
 
